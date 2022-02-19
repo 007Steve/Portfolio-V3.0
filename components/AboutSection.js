fcfc760460dacb4bf1d89d9b1e217de8fd3f69c1
007steve <stephen.plummer1.sp@gmail.com>
2022-02-19 12:10:46 -0500

    add about section

---
+++ components/AboutSection.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from "../styles/AboutSection.module.css";
+function AboutSection() {
+    return (
+        <div id="about" className={styles.aboutSection}>
+            
+        </div>
+    )
+}
+
+export default AboutSection
