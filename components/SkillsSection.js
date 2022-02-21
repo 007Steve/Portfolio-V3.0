125f9d3bf6eeac7f073705feafa46122b02e4132
007steve <stephen.plummer1.sp@gmail.com>
2022-02-21 18:25:25 -0500

    updated remove array

--- components/SkillsSection.js
+++ components/SkillsSection.js
@@ -2,51 +2,49 @@ import React from 'react'
 import styles from "../styles/SkillsSection.module.css";
 import SkillsCard from './SkillsCard';
 function SkillsSection() {
-    return (
-        <section className={styles.skillsSection}>
-            <h1 className={styles.skillsSection__title}>What I can do</h1>
-            <div className={styles.skillsSection__container}>
-                <SkillsCard
-             
-                  title="Front-End"
-                  img="/reactlogo.png"
-                  p={[
-                    key,
+  return (
+    <section className={styles.skillsSection}>
+      <h1 className={styles.skillsSection__title}>What I can do</h1>
+      <div className={styles.skillsSection__container}>
+        <SkillsCard
+          title="Front-End"
+          img="/reactlogo.png"
+          p={`
                     "Develop user-frienly website that are responsive with frameworks",
-                    <strong> {`React `}</strong>,
+                     "React" ,
                     "and ",
-                    <strong> NextJS </strong>,
-                  ]}
-                />
-                <SkillsCard
-                  title="Back-End"
-                  img="/DataIcon.png"
-                  color="orange"
-                  // p={[
-                  //   "Build and maintain the application ",
-                  //   <strong> Servers </strong>,
-                  //   "and ",
-                  //   <strong> Database</strong>,
-                  //   "with",
-                  //   <strong> Node JS</strong>,
-                  // ]}
-                  key={2}
-                />
-                <SkillsCard
-                  title="Spatial Web"
-                  img="/Web.png"
-                  color="purple"
-                  // p={[
-                  //   "Develop Advance website bult for the metaverse responsive with technologies",
-                  //   <strong> AR </strong>,
-                  //   "and ",
-                  //   <strong> VR </strong>,
-                  // ]}
-                  key={3}
-                /> 
-              </div>
-        </section>
-    )
+                    " NextJS" ,
+                  `}
+        />
+        <SkillsCard
+          title="Back-End"
+          img="/DataIcon.png"
+          color="orange"
+          p={`
+                    "Build and maintain the application ",
+                    " Servers ",
+                    "and ",
+                    " Database",
+                    "with",
+                   "  Node JS",`
+          }
+          key={2}
+        />
+        <SkillsCard
+          title="Spatial Web"
+          img="/Web.png"
+          color="purple"
+          p={`
+                    "Develop Advance website bult for the metaverse responsive with technologies",
+                     "AR ",
+                    "and ",
+                    " VR ",
+                  `}
+          key={3}
+        />
+      </div>
+    </section>
+  )
 }
 
 export default SkillsSection
