c9cb31670a8310be60a036690eefde1f2497c03f
007steve <stephen.plummer1.sp@gmail.com>
2022-02-21 16:41:16 -0500

    added project section

---
+++ components/ProjectSection.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styles from "../styles/ProjectSection.module.css";
+import ProjectCard from './ProjectCard';
+function ProjectSection() {
+  return (
+    <section id="portfolio" className={styles.projectsSection}>
+      <h1 className={styles.title}>Portfolio</h1>
+
+      <div className={styles.projectsSection__container}>
+        <ProjectCard color="music" img="/music_logo.png" />
+        <ProjectCard color="pink"/>
+        <ProjectCard color="chat" img="/laboratory-logo-vector-27258994.png"/>
+        <ProjectCard color="blue"/>
+      </div>
+    </section>
+  )
+}
+
+export default ProjectSection
