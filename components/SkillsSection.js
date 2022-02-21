c59aacf3a56f5a39d47e2618c0a92b2ffa6b277b
007steve <stephen.plummer1.sp@gmail.com>
2022-02-21 17:01:57 -0500

    updated fix

--- components/SkillsSection.js
+++ components/SkillsSection.js
@@ -10,37 +10,37 @@ function SkillsSection() {
                 key={1}
                   title="Front-End"
                   img="/reactlogo.png"
-                  p={[
-                    "Develop user-frienly website that are responsive with frameworks",
-                    <strong> React </strong>,
-                    "and ",
-                    <strong> NextJS </strong>,
-                  ]}
+                  // p={[
+                  //   "Develop user-frienly website that are responsive with frameworks",
+                  //   <strong> React </strong>,
+                  //   "and ",
+                  //   <strong> NextJS </strong>,
+                  // ]}
                 />
                 <SkillsCard
                   title="Back-End"
                   img="/DataIcon.png"
                   color="orange"
-                  p={[
-                    "Build and maintain the application ",
-                    <strong> Servers </strong>,
-                    "and ",
-                    <strong> Database</strong>,
-                    "with",
-                    <strong> Node JS</strong>,
-                  ]}
+                  // p={[
+                  //   "Build and maintain the application ",
+                  //   <strong> Servers </strong>,
+                  //   "and ",
+                  //   <strong> Database</strong>,
+                  //   "with",
+                  //   <strong> Node JS</strong>,
+                  // ]}
                   key={2}
                 />
                 <SkillsCard
                   title="Spatial Web"
                   img="/Web.png"
                   color="purple"
-                  p={[
-                    "Develop Advance website bult for the metaverse responsive with technologies",
-                    <strong> AR </strong>,
-                    "and ",
-                    <strong> VR </strong>,
-                  ]}
+                  // p={[
+                  //   "Develop Advance website bult for the metaverse responsive with technologies",
+                  //   <strong> AR </strong>,
+                  //   "and ",
+                  //   <strong> VR </strong>,
+                  // ]}
                   key={3}
                 /> 
               </div>
