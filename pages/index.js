702ea99b01237cd7ff8ad9899ea83025f920ae99
007steve <stephen.plummer1.sp@gmail.com>
2022-02-21 12:06:56 -0500

    updated with skills section

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import HeaderSection from '../components/HeaderSection'
 import AboutSection from '../components/AboutSection'
+import SkillsSection from '../components/SkillsSection'
+import ProjectSection from '../components/ProjectSection'
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -12,7 +14,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <HeaderSection />
-      <AboutSection/>
+      <AboutSection />
+      <SkillsSection />
+      <ProjectSection />
     </div>
   )
 }
