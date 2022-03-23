0b1b2dcda2ef087db5381063166c78fe49d6e72e
007steve <stephen.plummer1.sp@gmail.com>
2022-03-22 21:55:01 -0400

    removed img

--- components/SkillsCard.js
+++ components/SkillsCard.js
@@ -6,7 +6,7 @@ function SkillsCard({ title, img,p }) {
     <div className={styles.skillsCard}>
       <div className={`${styles.skillsCard__imgContainer}`}>
         <Image  className={styles.skillsCard__img }src={img} height={200} width={200} alt="a logo of project icon"/>
-        <img  alt="" />
+       
       </div>
       <h2 className={styles.skillsCard__title}>{title} </h2>
       <p className={styles.skillsCard__texts}>
