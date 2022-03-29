b90210aaea6ed1abf5c8424fee9f53b8ae61ceee
007steve <stephen.plummer1.sp@gmail.com>
2022-03-28 21:16:57 -0400

    loading state

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://www.stephenplummer.dev"></link>
       </Head>
-      {false ? <LandingPage /> : <>
+      {true ? <LandingPage /> : <>
         <HeaderSection />
         <AboutSection />
         <SkillsSection />
